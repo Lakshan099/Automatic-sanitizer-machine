f4182fb72c02f291ae10efe54e9900bd944c7e13
Lakshan Sandanayaka <81616253+Lakshan099@users.noreply.github.com>
2021-03-30 18:45:05 +0530

    Add files via upload

---
+++ SELA.ino
@@ -0,0 +1,31 @@
+
+#define pump 3
+#define svalve 5
+
+void setup() {
+  
+pinMode(pump,INPUT);
+pinMode(svalve,OUTPUT); 
+  
+
+}
+
+void loop() {
+
+ 
+ if (digitalRead(pump)==HIGH){
+
+ digitalWrite(svalve,HIGH);
+ 
+
+ }
+
+ else {
+  
+ digitalWrite(svalve,LOW);
+ 
+}
+
+  
+
+}
